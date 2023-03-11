24fff79138c681da74f7e85ed1e7743db94843b7
bhakti-chevli-dhiwise <bhakti.chevli@dhiwise.com>
2023-03-11 13:59:17 +0530

    initial commit

---
+++ Task-A/file-operations/src/Components/SimpleUpload.vue
@@ -0,0 +1,14 @@
+<template>
+  <form enctype="multipart/form-data">
+    <div class="field">
+      <label for="file" class="label">Upload File </label>
+      <input type="file" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SimpleUpload'
+}
+</script>
